5559f42277b5bea9072573456e98cbcedca7ca25
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-14 16:11:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
       if position_taken?(board, index) == false && (index >= 0 && index <= 8)
             true
-      else
+      elsif position_taken?(board, index) == true
             false
       end
 end
